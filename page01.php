46fa1dfd3d2e9198a4c1585fc3fccee73c6d3c73
Lucas Faislon Rodrigues <lucasfrbra@gmail.com>
2022-04-06 16:40:36 -0300

    Segunda Aula

---
+++ page01.php
@@ -0,0 +1,23 @@
+<?php
+
+//Cria uma Variável
+
+$carro = "Fusca";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $carro ?>
+
+    <?php //Utilizar o php para retirar os comentários do código fonte visível ao usuário// ?>
+    <img src="https://cdn.autopapo.com.br/box/uploads/2019/01/18210228/volkswagen-fusca-itamar-branco-lateral-732x488.jpg" alt="">
+</body>
+</html>
