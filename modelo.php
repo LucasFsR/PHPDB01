06d775ea9d8d4b3f448a562492843c7a2866f578
Lucas Faislon Rodrigues <lucasfrbra@gmail.com>
2022-04-08 16:46:47 -0300

    Aula 03

---
+++ modelo.php
@@ -0,0 +1,24 @@
+<?php
+
+//Inclui cabeçalho da página
+
+require("_header.php")
+
+?>
+            <section>
+                <h2>Título da Página</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus minus officiis enim nostrum laudantium sit eaque necessitatibus facere est natus. Odio expedita pariatur tempore nihil! Dolorum accusamus harum obcaecati cupiditate.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore reiciendis rerum dolores necessitatibus. Quasi, mollitia non! Culpa delectus ut error impedit, fuga numquam eveniet animi iste doloremque nostrum ad deserunt.</p>
+            </section>
+
+            <aside>
+                <h3>Lateral do Site</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sequi velit amet quam consectetur expedita laborum nisi facilis, itaque dolore alias cupiditate maiores voluptate debitis dolor quo delectus odio reprehenderit.</p>
+            </aside>
+<?php
+
+//Inclui o rodapé da página
+
+require("_footer.php")
+
+?>
